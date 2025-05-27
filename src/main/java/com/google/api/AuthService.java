7442ef2d013039289461b0aa9e8ab3c7b9fde242
Everton Cavalcante <evertonranielly@gmail.com>
2025-05-27 01:32:35 -0300

    Implementing automatic email sending with the GMail API

---
+++ src/main/java/com/google/api/AuthService.java
@@ -0,0 +1,40 @@
+package com.google.api;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+
+import java.io.*;
+import java.util.List;
+
+public class AuthService {
+    private static final String TOKENS_DIR_PATH = "auth";
+    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+    private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+    private static final String CREDENTIALS_FILE = "auth/credentials.json";
+
+    public Credential getCredentials(List<String> scopes) {
+         try {
+            GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
+                    new InputStreamReader(new FileInputStream(CREDENTIALS_FILE)));
+
+            GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT,
+                    JSON_FACTORY, clientSecrets, scopes)
+                    .setDataStoreFactory(new FileDataStoreFactory(new File(TOKENS_DIR_PATH)))
+                    .setAccessType("offline")
+                    .build();
+
+            LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+            return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/com/google/api/GMailService.java
@@ -0,0 +1,96 @@
+package com.google.api;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.List;
+import java.util.Properties;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.GmailScopes;
+import com.google.api.services.gmail.model.Message;
+import jakarta.activation.DataHandler;
+import jakarta.activation.FileDataSource;
+import jakarta.mail.MessagingException;
+import jakarta.mail.Session;
+import jakarta.mail.internet.*;
+
+public class GMailService {
+    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+    private static final NetHttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+    private static final String APPLICATION_NAME = "Gmail Java App";
+
+    private final AuthService authService;
+
+    public GMailService(AuthService authService) {
+        this.authService = authService;
+    }
+
+    public Gmail createGmailClient() {
+        List<String> scopes = List.of(GmailScopes.GMAIL_SEND);
+        Credential credential = authService.getCredentials(scopes);
+
+        return new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
+
+    public void sendEmailWithAttachment(String from, String to, String subject, String bodyText, File attachment) {
+        MimeMessage mimeMessage = createEmailWithAttachment(from, to, subject, bodyText, attachment);
+        Message message = createMessageWithEmail(mimeMessage);
+        Gmail gmailClient = createGmailClient();
+        try {
+            gmailClient.users().messages().send("me", message).execute();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private MimeMessage createEmailWithAttachment(String from, String to, String subject, String bodyText,
+                                                  File attachment) {
+        Session session = Session.getDefaultInstance(new Properties(), null);
+        MimeMessage email = new MimeMessage(session);
+        try {
+            email.setFrom(new InternetAddress(from));
+            email.addRecipient(MimeMessage.RecipientType.TO, new InternetAddress(to));
+            email.setSubject(subject);
+
+            MimeBodyPart bodyPart = new MimeBodyPart();
+            bodyPart.setText(bodyText);
+
+            MimeBodyPart attachmentPart = new MimeBodyPart();
+            FileDataSource fileDataSource = new FileDataSource(attachment);
+            attachmentPart.setDataHandler(new DataHandler(fileDataSource));
+            attachmentPart.setFileName(fileDataSource.getName());
+
+            MimeMultipart multipart = new MimeMultipart();
+            multipart.addBodyPart(bodyPart);
+            multipart.addBodyPart(attachmentPart);
+
+            email.setContent(multipart);
+            return email;
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Message createMessageWithEmail(MimeMessage emailContent) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try {
+            emailContent.writeTo(outputStream);
+            byte[] rawBytes = outputStream.toByteArray();
+            String encodedEmail = Base64.getUrlEncoder().encodeToString(rawBytes);
+
+            Message message = new Message();
+            message.setRaw(encodedEmail);
+            return message;
+        } catch (IOException | MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
